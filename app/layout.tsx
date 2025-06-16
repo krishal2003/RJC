13a28db2c725b77dae410398be7fcc65a5a81c09
Krishal Basnet <krishalbasnet@Krishals-Mac.local>
2025-06-16 15:00:14 +0545

    wqqqww

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,21 +1,22 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'RJC',
-  description: 'Created and Designed by Uddheshya Studio',
-  generator: 'react',
-  icons:'https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Rohit_J_Chettri.jpg/1200px-Rohit_J_Chettri.jpg'
-}
+  title: "Rohit John Chettri",
+  description: "Created and Designed by Uddheshya Studio",
+  generator: "react",
+  icons:
+    "https://upload.wikimedia.org/wikipedia/commons/thumb/7/74/Rohit_J_Chettri.jpg/1200px-Rohit_J_Chettri.jpg",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
